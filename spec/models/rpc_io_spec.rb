98bbcada07da52e8a1103fa6c30a43143d2e3243
timproctor <timandcamiproctor@gmail.com>
2017-06-29 09:18:15 -0600

    Move db to the correct dir

--- spec/models/rpc_io_spec.rb
+++ spec/models/rpc_io_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
-describe "RPC Input" do
-  it "is stored in database" do
-    
+describe "Rpc IO, the Input" do
+
+  before do
+    new_rpc = RpcIO.new
+  end
+
+  it "#write_input stores input value" do
+    STDIN = 5
+    # input = IO.pipe
+    # input.write("5")
+
+    new_rpc.write_input
+
 
 
     expect(test.input).to eq("5")
