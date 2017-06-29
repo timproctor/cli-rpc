967b83a45af2f5de6d1f3026b36fe7546188f9d4
timproctor <timandcamiproctor@gmail.com>
2017-06-29 08:10:19 -0600

    Add database file to house rpc values

--- models/input.rb
+++ models/input.rb
@@ -1,3 +1,12 @@
 class Input
-  
+  attr_reader :input
+
+  def initialize
+    puts "Reverse Polish Calculator is On"
+    puts "Please enter an integer:"
+    @input = gets.chomp
+  end
 end
+
+input = Input.new.input
+puts "you entered #{input} as an integer"
--- spec/models/input_spec.rb
+++ spec/models/input_spec.rb
@@ -3,5 +3,12 @@ require 'spec_helper'
 describe "Input" do
   it "must be in the form of an integer" do
     byebug
+    test = Input.new
+    send = IO.pipe
+    send.write("5")
+
+
+
+    expect(test.input).to eq("5")
   end
 end
