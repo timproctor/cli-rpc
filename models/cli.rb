1e37e52d49b824c38580f89bfcf3165f7be86ea4
timproctor <timandcamiproctor@gmail.com>
2017-07-03 19:16:01 -0600

    test an input and output from Cli

--- models/cli.rb
+++ models/cli.rb
@@ -1,15 +1,32 @@
 class CLI
-  attr_accessor :input, :output
-
-  def initialize
+  def self.run
     puts "Reverse Polish Calculator is On"
     puts "Please enter an integer:"
-    @input = gets.chomp
+    entry = gets
+    
+    print "Your number was "
+    puts entry
+    puts "Goodbye."
   end
 
-  def write_input
-    IO.write('./db/rpc_stack.rb', stack)
-  end
-
-
+  # def self.write_input(input)
+  #   stack = []
+  #   IO.write('./db/rpc_stack.rb', stack.push(input))
+  # end
+  #
+  # def self.get_input
+  #   input = gets.chomp
+  #   parse_input(input)
+  # end
+  #
+  # def self.parse_input(input)
+  #   if input != 'q'
+  #     write_input(input)
+  #   else
+  #     return
+  #   end
+  #   run
+  # end
 end
+
+CLI.run
--- spec/models/cli_spec.rb
+++ spec/models/cli_spec.rb
@@ -1,17 +1,7 @@
 require 'spec_helper'
 
-describe "Rpc IO, the Input" do
+describe "A CLI" do
 
   it "#write_input stores input value" do
-    new_rpc = RpcIO.new
-    byebug
-    input = IO.pipe
-    input.write("5")
-    new_rpc.write_input
-    stack = File.read('Users/timproctor/Documents/Projets/cli-rpc/db/rpc_stack.rb')
-
-
-
-    expect(stack.fist).to eq("5")
   end
 end
