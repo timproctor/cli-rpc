400f82c506a2322a13969dc28833aa0f79246781
timproctor <timandcamiproctor@gmail.com>
2017-07-18 08:14:48 -0600

    User notified of numeric inputs only

--- models/sanitizer.rb
+++ models/sanitizer.rb
@@ -15,15 +15,22 @@ class Sanitizer
     strip_spaces_off
     return false if has_quit?
     downcase_the_input
+    no_letters_please
     send_to_file
   end
 
   def downcase_the_input
     @input = @input.downcase
-    puts "from downcae_the_input #{@input}"
   end
 
   def no_letters_please
+    alphabet = ('a'..'z').to_a
+
+    alphabet.any? do |letter|
+      @input.include?(letter)
+    end
+
+    puts "RPC accepts only integers at this time. Please enter again: "
   end
 
   def has_quit?
