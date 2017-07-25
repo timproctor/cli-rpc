d0227a21f365e11818db200916cb5462256e8e65
timproctor <timandcamiproctor@gmail.com>
2017-07-25 08:13:15 -0600

    start a method that checks for operands if an operator is input

--- models/sanitizer.rb
+++ models/sanitizer.rb
@@ -38,14 +38,18 @@ class Sanitizer
   end
 
   def non_integers_present
-    pattern = %w(! @ # $ % ^ & ( ) _ ` ~ [ ] { } \ | : " ; ' , / ? > < )
+    pattern = %w(! @ # $ % ^ & ( ) _ ` ~ [ ] { } \ | : " ; ' , / ? > < =)
     pattern.any? do |symbol|
       if @input.include?(symbol)
         @input = ""
         puts "RPC accepts only integers at this time. Please enter again: "
       end
     end
+  end
 
+  def no_operators_before_operands
+    operators = %W(+ - * %)
+    
   end
 
   def has_quit?
