a44b07f7e979d20827f5121bb2b8b21450471bd3
timproctor <timandcamiproctor@gmail.com>
2017-07-25 12:22:28 -0600

    fix broken rewrite method

--- models/sanitizer.rb
+++ models/sanitizer.rb
@@ -1,14 +1,12 @@
 require_relative 'in_out'
 
 class Sanitizer
-  attr_accessor :input
+  OPERATORS = %W(+ - * %)
+  attr_accessor :input, :arrayed_store
 
   def initialize(input)
     @input = input
-  end
-
-  def strip_spaces_off
-    @input = @input.strip
+    @arrayed_store = (InOut.read).split(" ")
   end
 
   def clean
@@ -26,19 +24,38 @@ class Sanitizer
     end
   end
 
+  def send_to_file
+    InOut.write_to_file(@input)
+  end
+
+  def rewrite_to_file
+    arrayed_store.each { |number| InOut.write_to_files(number.to_s) }
+  end
+
   def operator_can_opperate
-    
+    operate if arrayed_store.size == 2
   end
 
-  def downcase_the_input
-    @input = @input.downcase
+  def operate
+    operand_l = arrayed_store[-2].to_f
+    operand_r = arrayed_store[-1].to_f
+
+    case @input
+    when "+"
+      @input = operand_l + operand_r
+      2.times{ arrayed_store.delete_at(-1) }
+      rewrite_to_file
+    else
+    end
   end
 
-  def letters_present
-    ("a".upto "z").any? do |letter|
-      if @input.include?(letter)
-        @input = ""
-        puts "RPC accepts only integers at this time. Please enter again: "
+  def operator_before_operand
+    OPERATORS.any? do |operator|
+      if @input.include?(operator)
+        if arrayed_store.size == 1
+          @input = ""
+          puts "Sorry, we need at least another number: "
+        end
       end
     end
   end
@@ -53,28 +70,24 @@ class Sanitizer
     end
   end
 
-  def operator_before_operand
-    operators = %W(+ - * %)
-
-    operators.any? do |operator|
-      if @input.include?(operator)
-        arrayed_store = (InOut.read).split(" ")
-        minimum_store = arrayed_store[0,2]
-        if minimum_store.size == 1
-          @input = ""
-          puts "Sorry, we need at least another number: "
-        end
+  def letters_present
+    ("a".upto "z").any? do |letter|
+      if @input.include?(letter)
+        @input = ""
+        puts "RPC accepts only integers at this time. Please enter again: "
       end
     end
   end
 
-
+  def downcase_the_input
+    @input = @input.downcase
+  end
 
   def has_quit?
     @input == 'q' || @input == 'quit'
   end
 
-  def send_to_file
-    InOut.write_to_file(@input)
+  def strip_spaces_off
+    @input = @input.strip
   end
 end
